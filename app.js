14e07de059a12cda0b759162c9200fe277141e65
mahima2001 <43854083+mahima2001@users.noreply.github.com>
2021-04-24 17:51:56 +0530

    ''

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ function UI(){
 }
 
 UI.prototype.addBookToList=function(book){
+    //console.log(book.title);
     const list=document.getElementById('book-list');
     const row=document.createElement('tr');
     row.innerHTML=`
@@ -19,16 +20,26 @@ UI.prototype.addBookToList=function(book){
     `;
     list.appendChild(row);
 }
+UI.prototype.clearFields=function(){
+    document.getElementById('title').value='';
+    document.getElementById('author').value='';
+    document.getElementById('isbn').value='';}
 
 
-document.getElementById('book-form').eventListener('submit',function(e){
-    const title=getElementById('title').value;
-    const author=getElementById('author').value;
-    const isbn=getElementById('isbn').value; 
-    
-const book=new Book(title,author,isbn);
+document.getElementById('book-form').addEventListener('submit',function(e){
+        // Get form values
+        const title = document.getElementById('title').value,
+              author = document.getElementById('author').value,
+              isbn = document.getElementById('isbn').value
+      
+        // Instantiate book
+        const book = new Book(title, author, isbn);
+      
+        // Instantiate UI
+        const ui = new UI();
+      
 
-    const ui=new UI();
     ui.addBookToList(book);
+    ui.clearFields();
     e.preventDefault();
 });
